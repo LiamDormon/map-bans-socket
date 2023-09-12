f6c334af72e2c12a76fb6a78e0e1f88c63191b84
Liam Dormon <66041893+Mojito-Fivem@users.noreply.github.com>
2023-09-12 16:34:50 +0100

    initial prototype

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const eSession = require('express-session')
+const { v4: uuidv4 } = require('uuid');
+const { Server } = require("socket.io")
+const sharedSession = require("express-socket.io-session")
+const cookieParser = require("cookie-parser")
+const { createServer } = require("node:http")
+const {join} = require("node:path")
+const crypto = require("node:crypto")
+const Room = require("./room")
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server);
+
+const secret = "ASfdASDgfASEDtSDgSDGsdfg34612asC"
+
+app.use(express.static(join(__dirname, "../public")))
+app.use(cookieParser(secret))
+
+const session = eSession({secret})
+app.use(session)
+io.use(sharedSession(session, {autoSave: true}))
+
+
+app.use((req, res, next) => {
+    if (!req.session.id) {  
+        req.session.id = uuidv4()
+    }
+    next()
+})
+
+app.get("/", (req, res) => {
+    res.status(200)
+})
+
+const rooms = new Map()
+
+app.get("/room", (req, res) => {
+    const {code} = req.query
+
+    const room = rooms.get(code)
+    if (!room) {
+        return res.sendStatus(404)
+    }
+
+    console.log(`${req.session.id} attempting to join room ${code}`)
+
+    if (!room.teamA) {
+        room.teamA = req.session.id
+    }
+
+    if (!room.teamB && room.teamA != req.session.id) {
+        room.teamB = req.session.id
+    }
+
+    console.log(room)
+
+    if (room.teamA !== req.session.id && room.teamB !== req.session.id) {
+        return res.sendStatus(403)
+    }
+
+    res.status(200).send("booya!")
+})
+
+io.on('connection', (socket) => {
+    socket.on("create-room", () => {
+        const roomId = crypto.randomBytes(4).toString("hex")
+        console.log(`${socket.handshake.session.id} created room ${roomId}`)
+
+        rooms.set(roomId, new Room())
+
+        socket.emit("successfully created", roomId)
+    })
+});
+
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
---
+++ server/room.js
@@ -0,0 +1,6 @@
+module.exports = class Room {
+    teamA = null
+    teamB = null
+
+    turn = 0; // 0 = Team A, 1 = Team B
+}
